fd2d3fcc9488122a00b39c6050b01b2c1e668685
Ghazal2623 <ghazal.ashraf24@gmail.com>
2025-08-11 23:52:17 +0400

    Update ReportsModule.java

--- src/ReportsModule.java
+++ src/ReportsModule.java
@@ -1,1 +1,24 @@
+import java.util.*;
+
+public class ReportsModule {
+
+    // Generate simple sales report from invoices
+    public void generateSalesReport(List<BillingModule.Invoice> invoices) {
+        double totalAmount = 0;
+        int paidCount = 0;
+        int unpaidCount = 0;
+
+        for (BillingModule.Invoice invoice : invoices) {
+            totalAmount += invoice.amount;
+            if (invoice.isPaid()) paidCount++;
+            else unpaidCount++;
+        }
+
+        System.out.println("----- Sales Report -----");
+        System.out.println("Total Invoices: " + invoices.size());
+        System.out.println("Paid Invoices: " + paidCount);
+        System.out.println("Unpaid Invoices: " + unpaidCount);
+        System.out.println("Total Amount: $" + totalAmount);
+    }
+}
 
